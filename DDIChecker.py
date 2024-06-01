bc6e4260deb372559631176e52d3eef82b188279
lahsai <adit.chintamaneni@gmail.com>
2024-05-31 21:45:35 -0700

    pushing final updated file

--- DDIChecker.py
+++ DDIChecker.py
@@ -2,19 +2,18 @@ import os
 import glob
 import pickle
 import copy
-import argparse
 import pandas as pd
-from keras.models import load_model
-# from sklearn.preprocessing import MultiLabelBinarizer
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import LabelBinarizer
 
-def predict_DDI(output_dir, output_file, ddi_input_file, trained_weight, threshold):  
+def make_prediction(output_dir, output_file, ddi_input_file, trained_model, threshold):  
     with open('./data/multilabelbinarizer.pkl', 'rb') as fid:
         lb = pickle.load(fid)
     
     df = pd.read_csv(ddi_input_file, index_col=0)    
     ddi_pairs = list(df.index)
     X = df.values    
-    model = load_model(trained_weight)
+    model = load_model(trained_model)
     y_predicted = model.predict(X)    
     original_predicted_ddi = copy.deepcopy(y_predicted)
     y_predicted[y_predicted >= threshold] = 1
@@ -22,13 +21,13 @@ def predict_DDI(output_dir, output_file, ddi_input_file, trained_weight, thresho
 
     y_predicted_inverse = lb.inverse_transform(y_predicted)   
     
-    fp = open(output_file, 'w')
-    print >>fp, 'Drug pair\tPredicted class\tScore'
-    for i in range(len(ddi_pairs)):
-        predicted_ddi_score = original_predicted_ddi[i]
-        predicted_ddi = y_predicted_inverse[i]
-        each_ddi = ddi_pairs[i]           
-        for each_predicted_ddi in predicted_ddi:
-            print >>fp, '%s\t%s\t%s'%(each_ddi, each_predicted_ddi, predicted_ddi_score[each_predicted_ddi-1])
-    fp.close()
+    with open(output_file, 'w') as fp:
+        fp.write('Drug pair\tPredicted class\tScore\n')
+        for i in range(len(ddi_pairs)):
+            predicted_ddi_score = original_predicted_ddi[i]
+            predicted_ddi = y_predicted_inverse[i]
+            each_ddi = ddi_pairs[i]           
+            for each_predicted_ddi in predicted_ddi:
+                fp.write('%s\t%s\t%s\n' % (each_ddi, each_predicted_ddi, predicted_ddi_score[each_predicted_ddi-1]))
+
 
